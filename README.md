fdc7d507642d234042a4ec4eaf985a622a0cf64e
Alexander Bocharov <alex.bocharov@outlook.com>
2021-04-13 16:33:28 +0300

    Add runner based on Ubuntu 20.04

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # GitHub Actions Runner 
 
 Docker images for GitHub Actions Runner.
+
+## Environment variables
+
+The following environment variables allows you to control the configuration parameters.
+
+| Name | Description | Required / Default value |
+| ---- | ----------- | ------------------------ |
+| RUNNER_NAME | Name of the runner displayed in the GitHub UI | Hostname of the container |
+| RUNNER_LABELS | Extra labels in addition to the default: 'self-hosted,Linux,X64' (based on your OS and architecture) | "" |
+| RUNNER_WORK_DIRECTORY | Runner's work directory | "_work" |
+| RUNNER_REPOSITORY_URL | The runner will be linked to this repository URL | Required if `RUNNER_ORGANIZATION_URL` is not provided |
+| RUNNER_ORGANIZATION_URL | The runner will be linked to this organization URL. *(Self-hosted runners API for organizations is currently in public beta and subject to changes)* | Required if `RUNNER_REPOSITORY_URL` is not provided |
+| GITHUB_ACCESS_TOKEN | Personal Access Token. Used to dynamically fetch a new runner token (recommended, see below). | Required if `RUNNER_TOKEN` is not provided. |
+| RUNNER_TOKEN | Runner token provided by GitHub in the Actions page. These tokens are valid for a short period. | Required if `GITHUB_ACCESS_TOKEN` is not provided |
---
+++ src/focal/amd64/docker-entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [[ "$@" == "bash" ]]; then
+  exec $@
+fi
+
+if [[ -z $RUNNER_NAME ]]; then
+  echo "RUNNER_NAME environment variable is not set, using '${HOSTNAME}'."
+  export RUNNER_NAME=${HOSTNAME}
+fi
+
+if [[ -z $RUNNER_GROUP ]]; then
+  echo "RUNNER_GROUP environment variable is nit set, using 'Default'."
+  export RUNNER_GROUP="Default"
+fi
+
+if [[ -z $RUNNER_WORK_DIRECTORY ]]; then
+  echo "RUNNER_WORK_DIRECTORY environment variable is not set, using '_work'."
+  export RUNNER_WORK_DIRECTORY="_work"
+fi
+
+if [[ -n $RUNNER_LABELS ]]; then
+  echo "RUNNER_LABELS environment variable is nit set, using 'Default'."
+  export RUNNER_LABELS="Default"
+fi
+
+if [[ -z $RUNNER_TOKEN && -z $GITHUB_ACCESS_TOKEN ]]; then
+  echo "Error : You need to set RUNNER_TOKEN (or GITHUB_ACCESS_TOKEN) environment variable."
+  exit 1
+fi
+
+if [[ -z $RUNNER_REPOSITORY_URL && -z $RUNNER_ORGANIZATION_URL ]]; then
+  echo "Error : You need to set the RUNNER_REPOSITORY_URL (or RUNNER_ORGANIZATION_URL) environment variable."
+  exit 1
+fi
+
+if [[ -f ".runner" ]]; then
+  echo "Runner already configured. Skipping config."
+else
+  if [[ ! -z $RUNNER_ORGANIZATION_URL ]]; then
+    SCOPE="orgs"
+    RUNNER_URL="${RUNNER_ORGANIZATION_URL}"
+  else
+    SCOPE="repos"
+    RUNNER_URL="${RUNNER_REPOSITORY_URL}"
+  fi
+
+  if [[ -n $GITHUB_ACCESS_TOKEN ]]; then
+
+    echo "Exchanging the GitHub Access Token with a Runner Token (scope: ${SCOPE})..."
+
+    _PROTO="$(echo "${RUNNER_URL}" | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    _URL="$(echo "${RUNNER_URL/${_PROTO}/}")"
+    _PATH="$(echo "${_URL}" | grep / | cut -d/ -f2-)"
+
+    RUNNER_TOKEN="$(curl -XPOST -fsSL \
+        -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+        -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/${SCOPE}/${_PATH}/actions/runners/registration-token" \
+        | jq -r '.token')"
+  fi
+
+  ./config.sh \
+      --url $RUNNER_URL \
+      --token $RUNNER_TOKEN \
+      --name $RUNNER_NAME \
+      --work $RUNNER_WORK_DIRECTORY \
+      --runnergroup $RUNNER_GROUP \
+      --labels $RUNNER_LABELS \
+      --unattended \
+      --replace
+fi
+
+exec "$@"
